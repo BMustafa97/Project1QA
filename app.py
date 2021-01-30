5ffaeb00c2a53d7dcad1edb475addf608520da7c
bmustafa97 <bilalmustafa1797@gmail.com>
2021-01-30 19:02:12 +0000

    Home Page + DB C2

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
 
 @app.route('/')
 def home():
-        return "Your Home Page"
+    todo_list = Todo.query.all()
+    print(todo_list)
+    return render_template('home.html', todo_list=todo_list)
 
 if __name__ == "__main__":
     app.run(debug=True)
