8e2a803b0b34c2a2e94460fb52d756c8e244cc52
bmustafa97 <bilalmustafa1797@gmail.com>
2021-02-03 22:01:47 +0000

    CRUD COMPLETE

--- app.py
+++ app.py
@@ -2,20 +2,28 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+
 
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
-@app.route('/')
+#class Name(db.Model):
+#    id = db.Column(db.Integer, primary_key=True, nullable=False)
+#    persons = db.Column(db.String(15))
+#    todos = db.relationship('Todo', backref='Name')
+#establish a relationship between this table and Todo one
+
+@app.route("/")
 def home():
     todo_list = Todo.query.all()
-    print(todo_list)
-    return render_template('home.html', todo_list=todo_list)
+    return render_template("home.html", todo_list=todo_list)
+
 
 @app.route("/add", methods=["POST"])
 def add():
@@ -25,12 +33,14 @@ def add():
     db.session.commit()
     return redirect(url_for("home"))
 
+
 @app.route("/update/<int:todo_id>")
 def update(todo_id):
     todo = Todo.query.filter_by(id=todo_id).first()
     todo.complete = not todo.complete
     db.session.commit()
     return redirect(url_for("home"))
+
 
 @app.route("/delete/<int:todo_id>")
 def delete(todo_id):
